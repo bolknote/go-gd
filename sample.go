8a2d713311d96e2c7c2cf647c67d18c610592052
Evgeny Stepanischev <imbolk@gmail.com>
2011-05-12 15:06:35 +0400

    Comments and typofiix

--- sample.go
+++ sample.go
@@ -7,10 +7,14 @@ func main() {
     defer pict.Destroy()
 
     color := pict.ColorAllocate(0, 0, 0)
+
+    // Non-Unicode font
     font := gd.GetFont(gd.FONTGIANT)
 
     pict.Char(font, 100, 100, "B", color)
     pict.String(font, 100, 120, "bolknote.ru", color)
+
+    // Unicode font
     pict.StringFT(color, "/Library/Fonts/Impact.ttf", 12, 0, 100, 150, "Hello! Привет!")
 
     pict.Jpeg("out.jpg", 95)
