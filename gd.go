b4b40f3998abeee4b64a9731bd1567c92152f559
Evgeny Stepanischev <imbolk@gmail.com>
2011-05-12 11:01:04 +0400

    Resize + Color allocation

--- gd.go
+++ gd.go
@@ -4,6 +4,7 @@ import "C"
 import "os"
 
 type Image struct {img C.gdImagePtr}
+type Color int
 
 func Create(sx, sy int) *Image {
     return &Image{img: C.gdImageCreate(C.int(sx), C.int(sy))}
@@ -43,5 +44,58 @@ func (p *Image) Jpeg(out string, quality int) {
 }
 
 func (p *Image) CopyResampled(dst Image, dstX, dstY, srcX, srcY, dstW, dstH, srcW, srcH int) {
-    C.gdImageCopyResampled(dst.img, p.img, C.int(dstX), C.int(dstY), C.int(srcX), C.int(srcY), C.int(dstW), C.int(dstH), C.int(srcW), C.int(srcH))
+    C.gdImageCopyResampled(dst.img, p.img, C.int(dstX), C.int(dstY), C.int(srcX), C.int(srcY),
+        C.int(dstW), C.int(dstH), C.int(srcW), C.int(srcH))
 }
+
+func (p *Image) Copy(dst Image, dstX, dstY, srcX, srcY, w, h int) {
+    C.gdImageCopy(dst.img, p.img, C.int(dstX), C.int(dstY), C.int(srcX), C.int(srcY), C.int(w), C.int(h))
+}
+
+func (p *Image) ColorAllocate(r, g, b int) Color {
+    return (Color)(C.gdImageColorAllocate(p.img, C.int(r), C.int(g), C.int(b)))
+}
+
+func (p *Image) ColorAllocateAlpha(r, g, b, a int) Color {
+    return (Color)(C.gdImageColorAllocateAlpha(p.img, C.int(r), C.int(g), C.int(b), C.int(a)))
+}
+
+func (p *Image) ColorClosest(r, g, b int) Color {
+    return (Color)(C.gdImageColorClosest(p.img, C.int(r), C.int(g), C.int(b)))
+}
+
+func (p *Image) ColorClosestAlpha(r, g, b, a int) Color {
+    return (Color)(C.gdImageColorClosestAlpha(p.img, C.int(r), C.int(g), C.int(b), C.int(a)))
+}
+
+func (p *Image) ColorClosestHWB(r, g, b int) Color {
+    return (Color)(C.gdImageColorClosestHWB(p.img, C.int(r), C.int(g), C.int(b)))
+}
+
+func (p *Image) ColorExact(r, g, b int) Color {
+    return (Color)(C.gdImageColorExact(p.img, C.int(r), C.int(g), C.int(b)))
+}
+
+func (p *Image) ColorExactAlpha(r, g, b, a int) Color {
+    return (Color)(C.gdImageColorExactAlpha(p.img, C.int(r), C.int(g), C.int(b), C.int(a)))
+}
+
+func (p *Image) ColorResolve(r, g, b int) Color {
+    return (Color)(C.gdImageColorResolve(p.img, C.int(r), C.int(g), C.int(b)))
+}
+
+func (p *Image) ColorResolveAlpha(r, g, b, a int) Color {
+    return (Color)(C.gdImageColorResolveAlpha(p.img, C.int(r), C.int(g), C.int(b), C.int(a)))
+}
+
+func (p *Image) ColorDeallocate(color Color) {
+    C.gdImageColorDeallocate(p.img, C.int(color))
+}
+
+
+
+
+
+
+
+
